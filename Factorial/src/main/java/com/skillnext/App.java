2495214756d189a302e8639f8a83332e460cf4b0
abdul-rouf-cyber <abdulroufqureshi98@gmail.com>
2025-12-15 10:41:22 +0530

    initial

---
+++ Factorial/src/main/java/com/skillnext/App.java
@@ -0,0 +1,19 @@
+package com.skillnext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+		int n=5;
+		int fact=1;
+		for(int i=1;i<=n;i++){
+			fact=fact*i;
+		}
+		System.out.println(fact);
+    }
+}
